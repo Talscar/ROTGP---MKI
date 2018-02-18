695f68670c78c48463ce43c14acc86b07b8ca2b4
AIE\s171*** <talscartalcom@gmail.com>
2018-02-18 11:04:16 +1030

    #New scene with some scripts on the way to make game function.
    #LobbyManager
    #movementController

---
+++ Assets/LobbyManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviour {
+
+    //Who is connecting?
+    //What are their characters?
+    //Is this lobby at capacity?
+    //Does this lobby have AI? If so may replace!
+
+
+	//// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
---
+++ Assets/movementController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementController : MonoBehaviour {
+
+    //I am the controller... but no one can just control me.
+    //I am given a controller at start by the game manager or if a player drops off the service I can be given a AI in it's place.
+    //While I am told to do actions, I will move things based on what inputs I have been given.
+    //Should no players be present the game will end according to the server rules.
+
+    //Functions for movement
+    //Controller/Gamepad/Touchpad/Mouse and Keyboard.
+
+    //Function for my score
+
+    //Function for my own interactions
+
+    //Function to use Powerups in my possession.
+
+
+    bool whileMovement;
+    void movement(bool moving)
+    {
+        whileMovement = moving;
+    }
+
+
+    void Update()
+    {
+        if(whileMovement)
+        {
+            //The player is moving
+
+            //The player is moving
+        }
+    }
+}
