d916028fe981eb4e9d7c94f66b195a57bb809222
AIE\s171*** <talscartalcom@gmail.com>
2018-02-21 10:14:19 +1030

    ###Major changes to scripts and replaced a script with another renaming the new script!

--- Assets/Scriptable objects/Unity/StatusEffect.cs
+++ Assets/Scriptable objects/Unity/StatusEffect.cs
@@ -1,46 +1,134 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-[CreateAssetMenu(menuName = "Abilities/StatusEffect")]
+[CreateAssetMenu(menuName = "Abilities/StatusEffects/Actor Scale")]
 public class StatusEffect : Ability {
 
+    //What abilitys will this have as Data structs
+    //enum to select any one of these Data structs and utilise the variables within. Locked based on the selection of the Enum we will have.
+    public struct fatness
+    {
 
-    //I want a drop menu that selects a specific set of abilitys.
-    //I want to then have in the inspector a way to select an ability with variables specific to it's type.
-    //Then I want to be able to set these variables via a single script process using either a struct or other process!
-    //From there, I want the script that uses this derived from Mono behavior to then process the effects either been inflicted on self or inflicted on another!
-    //public[] Ability statusEffect;
-    public List<StatusEffect> Effect_Stacked;
+    }
+
+    public bool hasScaleAlteration = false;
+    [Tooltip("1 is equal to 100%. So 0 is equal to 0% and 2 = 200%. \n This value sets the current scale of something by a percentage.")]
+    [Range(0f, 2f)]
+    public float scalePercent = 1f;
+
+    //Case statement and Enums to control what is shown and what is processed. Less than, Greater than and Equal too.
+
+    //bool hasSpeedAlteration = false;
+    //[Tooltip("1 is equal to 100%. So 0 is equal to 0% and 15 = 1500%. \n This value sets the current speed of something by a percentage." )]
+    //[Range(0f, 15f)]
+    //public float speedPercent = 1f;
 
 
 
 
+    [Header("Global values ")]
+    [Tooltip("If duration it lasts over time, else it will only run once.")]
+    public bool hasDuration = false;
+    [Tooltip("How many seconds this ability will run for!")]
+    [Range(1f, 15f)]
+    public float duration = 1f;
 
 
+    //////public float projectileForce = 500f;
+    //////public Rigidbody projectile;
 
+    //////private ProjectileShootTriggerable launcher;
 
-    //[Header("Status effects")]
-    //public float fatness = 0.01f;
-    //public float speed = 0.99f;
-    //public float projectileForce = 500f;
-    //public Rigidbody projectile;
-
-    //private ProjectileShootTriggerable launcher;
-    private movementController player;
+    //void OnInspectorGUI()
+    //{
+    //    var Actor_Scale = Target as Actor_Scale;
+    //}
 
     public override void Initialize(GameObject obj)
     {
-        player = obj.GetComponent<movementController>();
-
-        //launcher = obj.GetComponent<ProjectileShootTriggerable>();
-        //launcher.projectileForce = projectileForce;
-        //launcher.projectile = projectile;
+        //////launcher = obj.GetComponent<ProjectileShootTriggerable>();
+        //////launcher.projectileForce = projectileForce;
+        //////launcher.projectile = projectile;
     }
 
     public override void TriggerAbility()
     {
-        player.statusProcessor(this);
-        //launcher.Launch();
+        //////launcher.Launch();
+    }
+
+
+}
+
+
+[CustomEditor(typeof(StatusEffect))]
+//[ca]
+public class Editor_ActorScale : Editor
+{
+    SerializedProperty abilityName;
+    SerializedProperty abilitySprite;
+    SerializedProperty abilitySound;
+    SerializedProperty abilityBaseCoolDown;
+
+    //
+    SerializedProperty Actor_Scale;
+    SerializedProperty hasScaleAlteration;// = false;
+    SerializedProperty scalePercent;
+
+    SerializedProperty hasDuration;
+    SerializedProperty duration;
+
+
+    public void OnEnable()
+    {
+        abilityName = serializedObject.FindProperty("abilityName");
+        abilitySprite = serializedObject.FindProperty("abilitySprite");
+        abilitySound = serializedObject.FindProperty("abilitySound");
+        abilityBaseCoolDown = serializedObject.FindProperty("abilityBaseCoolDown");
+
+
+        Actor_Scale = serializedObject.FindProperty("Actor_Scale");
+        hasScaleAlteration = serializedObject.FindProperty("hasScaleAlteration");
+        hasDuration = serializedObject.FindProperty("hasDuration");
+        scalePercent = serializedObject.FindProperty("scalePercent");
+        duration = serializedObject.FindProperty("duration");
+        //lookAtPoint = serializedObject.FindProperty("lookAtPoint");
+    }
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        //[Header("")]
+
+        EditorGUILayout.PropertyField(abilityName);
+        EditorGUILayout.PropertyField(abilitySprite);
+        EditorGUILayout.PropertyField(abilitySound);
+        EditorGUILayout.PropertyField(abilityBaseCoolDown);
+        EditorGUILayout.Separator();
+
+        Debug.LogWarning("Any modifications to script will have to be added upon requiring modification!");
+        EditorGUILayout.PropertyField(hasScaleAlteration);
+        if(hasScaleAlteration.boolValue)
+        {
+                    EditorGUILayout.PropertyField(scalePercent);
+        }
+        EditorGUILayout.PropertyField(hasDuration);
+        if(hasDuration.boolValue)
+        {
+            EditorGUILayout.PropertyField(duration);
+        }
+
+        //EditorGUILayout.PropertyField(Actor_Scale);
+
+        //if(Actor_Scale.floatValue.)
+        //if (lookAtPoint.vector3Value.y > (target as LookAtPoint).transform.position.y)
+        //{
+        //    EditorGUILayout.LabelField("(Above this object)");
+        //}
+        //EditorGUILayout.FloatField(Actor_Scale.)
+
+
+        serializedObject.ApplyModifiedProperties();
     }
 }
+//[custom]
---
+++ Assets/pickUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUp : MonoBehaviour {
+
+    [Tooltip("Could be anything from a script to process too a item to replace something else. \n Prefrably a scriptable GameObject")]
+    public Object[] Pickup;	
+    // Use this for initialization
+	void Start () {
+        //Works with all scripts based on what values collection passes onto scripts or pickup
+
+        foreach (Object runMe in Pickup) //Multiple items to run and use
+        {
+            if (runMe is StatusEffect)
+            {
+                Debug.Log("Process this thing: " + runMe);
+            }
+            Debug.Log("what is thing?  " + runMe);
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
