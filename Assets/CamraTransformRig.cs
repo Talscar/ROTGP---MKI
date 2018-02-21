74f66a3597f882d646f0292a5c7bd8db8b7ced08
Talscar-PC\Talscar <talscartalcom@gmail.com>
2018-02-21 22:05:02 +1030

    #Scene scripting and other

--- Assets/CamraTransformRig.cs
+++ Assets/CamraTransformRig.cs
@@ -6,6 +6,7 @@ public class CamraTransformRig : MonoBehaviour {
     public Transform followThis;
     public Vector3 vectorTransform;
     public Vector3 vectorRotation;
+    public float drag;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,6 +14,14 @@ public class CamraTransformRig : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //Follow the transform and maintain a certain speed.
+        if (followThis != null)
+        {
+            transform.position = followThis.position + vectorTransform;
+            //Quaternion rotation = Quaternion.LookRotation(followThis.position);
+            //transform.rotation = rotation;
+            //transform.rotation = 
+        }
 		
 	}
 }
