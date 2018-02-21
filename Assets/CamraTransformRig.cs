1eddd0cebadefeb720409cac00bc24a310e94dd4
Talscar-PC\Talscar <talscartalcom@gmail.com>
2018-02-21 22:21:21 +1030

    #Script

---
+++ Assets/CamraTransformRig.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamraTransformRig : MonoBehaviour {
+    public Transform followThis;
+    public Vector3 vectorTransform;
+    public Vector3 vectorRotation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
