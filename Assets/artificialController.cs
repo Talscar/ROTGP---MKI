60b14a9a93c6ecd1cee4aa92fbfcd6af233c0652
Talscar-PC\Talscar <talscartalcom@gmail.com>
2018-02-26 06:20:00 +1030

    #AI -Some other stuff - Clunky gameplay :D

--- Assets/artificialController.cs
+++ Assets/artificialController.cs
@@ -13,21 +13,74 @@ public class artificialController : MonoBehaviour {
      * 
      * Does have Ability?
     */
+    public Transform players;
+    public Transform containerObjectives;
     public Transform target;
 
-    private NavMeshAgent agent;
+    [SerializeField]private Vector3 moveToHere;
+    [SerializeField]private Vector3 node; 
+
+    //private NavMeshAgent agent;
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        NavMeshPath path = new NavMeshPath();
-        agent.CalculatePath(target.position, path);
-        if (path.status == NavMeshPathStatus.PathPartial)
-        {
-        }
+        if (GetComponent("movementController") != null)
+            myController = GetComponent<movementController>();
+
+        //agent = GetComponent<NavMeshAgent>();
+        //NavMeshPath path = new NavMeshPath();
+        //agent.CalculatePath(target.position, path);
+        //if (path.status == NavMeshPathStatus.PathPartial)
+        //{
+        //}
     }
 
-    void repath()
+    Transform getTarget(Transform newTarget)
     {
+        if(containerObjectives != null)
+        {
+            if(containerObjectives.GetComponentInChildren<Transform>() != null)
+            {
+                bool first = true;
+                //Transform newTarget;
+                float distance = float.MaxValue;
+                Transform[] targets = containerObjectives.GetComponentsInChildren<Transform>();
+                foreach(Transform child in targets)
+                {
+                    float testDistance = Vector3.Distance(child.position, transform.position);
+                    if(first)
+                    {
+                        newTarget = child;
+                        distance = testDistance;
+                    }
+                    else
+                    {
+                        if(testDistance < distance)
+                        {
+                            newTarget = child;
+                            distance = testDistance;
+                        }
+                    }
+                }
+            }
+        }
+        return newTarget;
+    }
+
+
+    /// <summary>
+    /// Using Primitives.
+    /// </summary>
+    void repath(Vector3 currentMoveLocation)
+    {
+
+        Vector3 targetDirection =  target.position - transform.position;
+        var distance = targetDirection.magnitude;
+
+        var direction = targetDirection / distance;
+        //Debug.Log(direction);
+
+        dir = new Vector2(direction.x, direction.z);
+        //Debug.Log(targetDirection);
         //https://docs.unity3d.com/Manual/editor-CustomEditors.html
         //Raycasts will identify a target in front obstructing the player.
         //Target blocking play will be messured to a point the player could be diverted.
@@ -37,6 +90,7 @@ public class artificialController : MonoBehaviour {
         //If can not reach location then try finding spaces at the object bounds.
         // Once found, recheck the path and if player can reach objective then go!
         //If not repeat previous steps.
+        return;
     }
     //Unity AI NavMesh
     bool RandomPoint(Vector3 center, float range, out Vector3 result)
@@ -60,13 +114,36 @@ public class artificialController : MonoBehaviour {
     public movementController myController;
     //public 
 
-	// Use this for initialization
-	//void Start () {
-		
-	//}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    //void Start () {
+
+    //}
+
+    // Update is called once per frame
+    Vector2 dir;
+	void Update ()
+    {
+        if (myController != null)
+        {
+            if(myController.identifier_IsAI)
+            {
+                Debug.Log("Check!");
+                if (target != null)
+                {
+                    repath(target.position);
+                    //Debug.Log(dir);
+                    myController.getNewDirection(dir);
+                }//myController.dir
+                else
+                {
+                    Debug.Log("Check! ELSE");
+                    dir.x = 0;
+                    dir.y = 0;
+                    myController.getNewDirection(dir);
+                    target = getTarget(null);
+                }
+            }
+        }
 		
 	}
 }
--- Assets/movementController.cs
+++ Assets/movementController.cs
@@ -33,80 +33,98 @@ public class movementController : MonoBehaviour {
     {
 
     }
+    //public bool allowMovement = false;
+    public static bool canMove = true;
 
     public float forceFactor = 10f;
     public float speed = 10f;
-    float rotSpeed = 15f;
+    public float rotSpeed = 15f;
     //float moveSpeed;
+    public void getNewDirection(Vector2 theDirection)
+    {
+        dirAi = theDirection;
+    }
+    Vector2 dirAi = new Vector2(0f, 0f); //Simulated by he Ai;
     void Update()
     {
-        float x = 0f;// = Input.GetAxis("Horizontal");// * Time.deltaTime * 150.0f;
-        float z = 0f;
-        rb.velocity = new Vector3(0, rb.velocity.y, 0);
-        if (!identifier_IsAI)
-        {
-            x = Input.GetAxis("Horizontal");// * Time.deltaTime * 150.0f;
-            z = Input.GetAxis("Vertical");// * Time.deltaTime * 3.0f) * speed;
-        }
-
-        Vector3 dir = new Vector3(x, 0, z);
-
-        //         1
-        //         |
-        //         |
-        // -1______+______ 1
-        //         |
-        //         |
-        //        -1
-
-        Vector3 targetDir = (dir + transform.position) - transform.position;
-        float step = rotSpeed * Time.deltaTime;
-        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
-        Debug.DrawRay(transform.position, newDir, Color.red);
-        transform.rotation = Quaternion.LookRotation(newDir);
-        //float moveDir ;
-        float mF = 0f;
-        if (z != 0 || x != 0)
-        {
-            float newZ = Mathf.Abs(z);
-            float newX = Mathf.Abs(x);
-            if(newZ > newX)
-            {
-                mF = newZ;
-            }
-            else
-            {
-                mF = newX;
-            }
-            mF = mF * speed;
-            //mF = newZ * newX;
-            //mF
-        }
-        //else if(z < 0 || x < 0)
+        //if(allowMovement && !canMove)
         //{
-        //    mF = -1f;
+        //    canMove = true;
         //}
+        //else
+   
+        if (canMove)
+        { //Can move based on Game Start Timer.
+            float x = dirAi.x;// = Input.GetAxis("Horizontal");// * Time.deltaTime * 150.0f;
+            float z = dirAi.y;
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+            if (!identifier_IsAI)
+            {
+                x = Input.GetAxis("Horizontal");// * Time.deltaTime * 150.0f;
+                z = Input.GetAxis("Vertical");// * Time.deltaTime * 3.0f) * speed;
+            }
 
-        rb.velocity += transform.forward * ((mF * Time.deltaTime * 3.0f) * forceFactor);
-        //transform.Rotate(0, x, 0);
-        ////transform.Translate(0, 0, z);
+            Vector3 dir = new Vector3(x, 0, z);
+
+            //         1
+            //         |
+            //         |
+            // -1______+______ 1
+            //         |
+            //         |
+            //        -1
+
+            Vector3 targetDir = (dir + transform.position) - transform.position;
+            float step = rotSpeed * Time.deltaTime;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+            Debug.DrawRay(transform.position, newDir, Color.red);
+            transform.rotation = Quaternion.LookRotation(newDir);
+            //float moveDir ;
+            float mF = 0f;
+            if (z != 0 || x != 0)
+            {
+                float newZ = Mathf.Abs(z);
+                float newX = Mathf.Abs(x);
+                if (newZ > newX)
+                {
+                    mF = newZ;
+                }
+                else
+                {
+                    mF = newX;
+                }
+                mF = mF * speed;
+                //mF = newZ * newX;
+                //mF
+            }
+            //else if(z < 0 || x < 0)
+            //{
+            //    mF = -1f;
+            //}
+
+            rb.velocity += transform.forward * ((mF * Time.deltaTime * 3.0f) * forceFactor);
+            //transform.Rotate(0, x, 0);
+            ////transform.Translate(0, 0, z);
 
 
-        //Vector3 moveDirection = new Vector3(0, 0, z);
-        ////rb.MovePosition(moveDirection * speed);//(Vector3.forward * (speed * z));
-        ////rb.AddForce(Vector3.forward * 1000);
-        //rb.velocity += transform.forward * (z * forceFactor);
+            //Vector3 moveDirection = new Vector3(0, 0, z);
+            ////rb.MovePosition(moveDirection * speed);//(Vector3.forward * (speed * z));
+            ////rb.AddForce(Vector3.forward * 1000);
+            //rb.velocity += transform.forward * (z * forceFactor);
 
-        if (inflicted != null)
-        {
+            if (inflicted != null)
+            {
 
-        }
-        //////Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"),Input.GetAxis("Vertical"));
+            }
+            //////Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"),Input.GetAxis("Vertical"));
 
-        if (inflicted != null)
-        {
-            inflicted.TriggerAbility();
-        }
+            if (inflicted != null)
+            {
+                inflicted.TriggerAbility();
+            }
+        } //canMove (Based on game start timer)
+
+
         //if(Input.GetKeyDown(KeyCode.Alpha4))
         //{
         //    inflicted.TriggerAbility();
@@ -134,19 +152,21 @@ public class movementController : MonoBehaviour {
     }
     public void statusProcessor(StatusEffect.dataTypes data, Ability _this)
     {
-        switch(data)
-        {
-            case StatusEffect.dataTypes.scale:
-                gameObject.transform.localScale = transform.localScale * inflicted.Scale.scalePercent;
-                break;
+        if (inflicted != null)
+            switch (data)
+            {
+                case StatusEffect.dataTypes.scale:
+                    gameObject.transform.localScale = transform.localScale * inflicted.Scale.scalePercent;
+                    break;
 
-            case StatusEffect.dataTypes.speed:
-                speed = speed * inflicted.Speed.speedPercent;
-                break;
+                case StatusEffect.dataTypes.speed:
+                    speed = speed * inflicted.Speed.speedPercent;
+                    break;
 
-            case StatusEffect.dataTypes.knockback:
-                break;
-        }
+                case StatusEffect.dataTypes.knockback:
+                    break;
+            }
+
         Debug.Log("Processing the StatusEffect!");
         inflicted = null;
 
